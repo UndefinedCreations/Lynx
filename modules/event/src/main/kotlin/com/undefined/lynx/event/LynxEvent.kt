b02d2ca01937c4723b1336927beacbbee624aa4c
lijnb <lijnbelt13@gmail.com>
2025-07-07 23:51:59 +0200

    feat(logger): add java support

--- modules/event/src/main/kotlin/com/undefined/lynx/event/LynxEvent.kt
+++ modules/event/src/main/kotlin/com/undefined/lynx/event/LynxEvent.kt
@@ -1,6 +1,5 @@
 package com.undefined.lynx.event
 
-import org.bukkit.Bukkit
 import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
--- modules/logger/src/main/kotlin/com/undefined/lynx/logger/LoggerUtil.kt
+++ modules/logger/src/main/kotlin/com/undefined/lynx/logger/LoggerUtil.kt
@@ -2,8 +2,16 @@ package com.undefined.lynx.logger
 
 import com.undefined.lynx.LynxConfig
 
-fun sendError(message: String) = LynxConfig.javaPlugin.logger.severe(message)
-fun sendWarn(message: String) = LynxConfig.javaPlugin.logger.warning(message)
-fun sendInfo(message: String) = LynxConfig.javaPlugin.logger.info(message)
-fun sendDebug(message: String) = LynxConfig.javaPlugin.logger.config(message)
-fun sendTrace(message: String) = LynxConfig.javaPlugin.logger.fine(message)
+fun sendError(message: String) = LoggerUtil.sendError(message)
+fun sendWarn(message: String) = LoggerUtil.sendWarn(message)
+fun sendInfo(message: String) = LoggerUtil.sendInfo(message)
+fun sendDebug(message: String) = LoggerUtil.sendDebug(message)
+fun sendTrace(message: String) = LoggerUtil.sendTrace(message)
+
+object LoggerUtil {
+    fun sendError(message: String) = LynxConfig.javaPlugin.logger.severe(message)
+    fun sendWarn(message: String) = LynxConfig.javaPlugin.logger.warning(message)
+    fun sendInfo(message: String) = LynxConfig.javaPlugin.logger.info(message)
+    fun sendDebug(message: String) = LynxConfig.javaPlugin.logger.config(message)
+    fun sendTrace(message: String) = LynxConfig.javaPlugin.logger.fine(message)
+}
